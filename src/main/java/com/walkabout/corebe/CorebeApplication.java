601642c0fab7e5c459a7836ec73fcb27f0ed0fc7
Thiago <thiago.andreolli@tivit.com>
2018-07-30 17:52:30 -0300

    project bootstrap

---
+++ src/main/java/com/walkabout/corebe/CorebeApplication.java
@@ -0,0 +1,12 @@
+package com.walkabout.corebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorebeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/walkabout/corebe/CorebeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.walkabout.corebe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorebeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
